d8138ae17855120c1646b8782aa9c36a3c60027c
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-07-06 17:56:54 -0400

    Added proto to main.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,13 @@
+use tonic::transport::Server;
+use tonic:: { Request, Response, Status, Streaming};
+
+use ogtemplate::chat_req_server::{ChatReq,ChatReqServer };
+use ogtemplate::{ Empty,Msg, Req};
+
+pub mod ogtemplate {
+    tonic::include_proto!("ogtemplate");
+}
+
 fn main() {
     println!("Hello, world!");
 }
