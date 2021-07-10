dc6fa8b1a17d0727a4acd1ea8d221929e6caf5fe
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-07-10 18:03:00 -0400

    sinusoidal wave

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -78,8 +78,8 @@ impl ChatReq for Service {
         let msg = Msg { user_name,content };
         let mut x:Vec<f32> = Vec::with_capacity(5000);
         let mut y:Vec<f32> = Vec::with_capacity(5000);
-        (0..5000).for_each(|i| x.push(rand::thread_rng().gen_range(0.0..10.0)));
-        (0..5000).for_each(|i| y.push(rand::thread_rng().gen_range(0.0..10.0)));
+        (0..5000).for_each(|i| x.push(i as f32));
+        (0..5000).for_each(|i| y.push(f32::sin(rand::thread_rng().gen_range(0.0..1.0))));
         Ok(Response::new(ResponseArray{
             x ,
             y,
